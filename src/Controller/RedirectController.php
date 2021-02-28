e1a2fb207d0512234e49816730308eace6adad20
MouradBENRAHAL <79028897+MouradBENRAHAL@users.noreply.github.com>
2021-02-28 17:29:39 +0100

    Initial commit

---
+++ src/Controller/RedirectController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RedirectController extends AbstractController
+{
+
+    /**
+     * @Route("/frontacceuil", name="frontacceuil")
+     */
+    public function frontend(): Response
+    {
+        return $this->render('frontend/frontbody.html.twig', [
+            'controller_name' => 'RedirectController',
+        ]);
+    }
+    /**
+     * @Route("/backacceuil", name="backacceuil")
+     */
+    public function backend(): Response
+    {
+        return $this->render('backend/backbody.html.twig', [
+            'controller_name' => 'RedirectController',
+        ]);
+    }
+
+}
